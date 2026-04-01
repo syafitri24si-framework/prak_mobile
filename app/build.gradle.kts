b8d790c02e554cc7c2a116a8e41143213c7e4a26
Aulia <syafitri24si@mahasiswa.pcr.ac.id>
2026-04-01 08:42:09 +0700

    sc commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+
+    }
 }
 
 dependencies {
---
+++ app/src/main/java/com/example/kerenapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,42 @@
+package com.example.kerenapps.pertemuan_3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kerenapps.R
+import com.example.kerenapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_third)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inisialisasi komponen
+//        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+//        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+            val noTujuan = binding.inputNoTujuan.text.toString()
+
+            Toast.makeText(this, "Pesan berhasil dikirim $noTujuan", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
