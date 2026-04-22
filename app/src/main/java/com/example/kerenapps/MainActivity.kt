a4db630a7135ecb7a5f9d145d9eafde3332fee96
Aulia <syafitri24si@mahasiswa.pcr.ac.id>
2026-04-22 09:57:28 +0700

    Pertemuan6

--- app/src/main/java/com/example/kerenapps/MainActivity.kt
+++ app/src/main/java/com/example/kerenapps/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kerenapps
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,21 +9,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.kerenapps.databinding.ActivityFourthBinding
 import com.example.kerenapps.databinding.ActivityMainBinding
 import com.example.kerenapps.databinding.ActivityThirdBinding
+import com.example.kerenapps.pertemuan_2.SecondActivity
 import com.example.kerenapps.pertemuan_3.ThirdResultActivity
 import com.example.kerenapps.pertemuan_4.FourthActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -30,13 +36,36 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.btnToFourth.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            intent.putExtra("name", "Politeknik Caltex Riau")
-            intent.putExtra("asal", "Rumbai")
-            intent.putExtra("usia", 25)
+
+        binding.btnLogout.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
-            finish()
+        }
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+        binding.btnLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi Logout")
+                .setMessage("Apakah Anda yakin ingin logout?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    val editor = sharedPref.edit()
+                    editor.clear()
+                    editor.apply()
+                    dialog.dismiss()
+                    Log.e("Logout", "User logout")
+                    finish()
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+            binding.btnToFourth.setOnClickListener {
+                val intent = Intent(this, FourthActivity::class.java)
+                intent.putExtra("name", "Politeknik Caltex Riau")
+                intent.putExtra("asal", "Rumbai")
+                intent.putExtra("usia", 25)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
